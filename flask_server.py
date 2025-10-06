23036e43213d5b0321766395c512d556e46756bd
Kerjing <jing2003038@gmail.com>
2025-10-07 01:24:04 +0800

    ec2 flask

--- flask_server.py
+++ flask_server.py
@@ -187,5 +187,4 @@ def ask_question_stream():
 
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 3000))
-    serve(app, host="0.0.0.0", port=5000)
+    serve(app, host="0.0.0.0", port=3000)
